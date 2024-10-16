89c7b0d7a6e6ade0501da030c87a7def54dbc515
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2024-10-16 16:03:15 +0530

    feat: created backend for meetup app

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+const corsOptions ={
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
+
+const {initializeDatabase} = require('./db/db.connect')
+const Meetup = require('./models/meetup.model')
+
+app.use(express.json())
+
+initializeDatabase()
+
+app.get("/", (req,res)=>{
+    res.send("Hello Express!")
+})
+
+async function createEvent(newEvent){
+    try {
+        const events = new Meetup(newEvent)
+        const saveEvents = await events.save()
+        return saveEvents
+    } catch (error) {
+     console.log(error)   
+    }
+}
+
+app.post("/events", async (req,res)=>{
+   try {
+    const events = await createEvent(req.body)
+    res.status(201).json({message: "New event is created.", event: events})
+
+   } catch (error) {
+        res.status(500).json({error: "Failed to create event."})
+   }
+})
+
+async function findAllEvent(){
+    try {
+       const events = await Meetup.find()
+       return events 
+    } catch (error) {
+     console.log(error)   
+    }
+}
+
+app.get("/events", async(req,res)=>{
+    try {
+        const events = await findAllEvent()
+        if(events.length !=0){
+            res.json(events)
+        }else{
+            res.status(404).json({error: "Event not found."})
+        }
+    } catch (error) {
+     res.status(500).json({error: "Failed to get all events."})   
+    }
+})
+
+async function findEventByTitle(eventTitle){
+    try {
+        const events = await Meetup.findOne({title: eventTitle})
+        return events
+    } catch (error) {
+    console.log(error)       
+    }
+}
+
+app.get("/events/:eventTitle", async (req,res)=> {
+    try{
+        const events = await findEventByTitle(req.params.eventTitle)
+        if(events){
+            res.json(events)
+        }else{
+            res.status(404).json({error: "Event not found"})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to find event."})
+    }
+})
+
+async function findEventByTags(tagName){
+    try {
+        const events = await Meetup.findOne({eventTags: tagName})
+        return events
+    } catch (error) {
+        console.log(error)        
+    }
+}
+
+app.get("/events/tags/:tagName", async(req,res)=>{
+    try {
+        const events = await findEventByTags(req.params.tagName)
+        if(events){
+            res.json(events)
+        }else{
+            res.status(404).json({error: "Event not found"})
+        }
+    } catch (error) {
+        res.status(500).json({error: "Failed to find event."})
+    }
+})
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port', PORT)
+})
+module.exports = app
---
+++ models/meetup.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const meetupSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    typeOfEvent: {
+      type:  String,
+      required: true,
+      default: "Both",
+      enum: ["Online","Offline","Both"]
+    },
+    hostedBy: {
+        type: String,
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime:{
+        type: String,
+       
+    },
+    address: {
+        type: String,
+    },
+    price: {
+        type: String,
+    },
+    speakers : [{
+        type: String,
+
+    }],
+    details: {
+        type: String,
+    },
+    dressCode: {
+        type: String,
+    },
+    ageRestriction: {
+        type: String,
+    },
+    eventTags:[ {
+        type: String,
+    }],
+    imageUrl : {
+        type: String,
+    },
+
+},{timestamps: true})
+
+const Meetup = mongoose.model("Meetup", meetupSchema)
+module.exports = Meetup
